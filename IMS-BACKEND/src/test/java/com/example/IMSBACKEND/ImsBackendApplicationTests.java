485be2ae3a545826e56181a1d2b4b652e8dc1ac1
johnsagmani@gmail.com <johnsagmani@gmail.com>
2024-01-10 19:14:55 -0800

    Initialized Repo

---
+++ IMS-BACKEND/src/test/java/com/example/IMSBACKEND/ImsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IMSBACKEND;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
