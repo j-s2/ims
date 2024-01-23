828a634bd452f2b319744bd4757f1498f66c720c
smani-220 <kobemcgoat24@gmail.com>
2024-01-22 17:09:38 -0800

    Began frontend implementation

--- IMS-FRONTEND/src/pages/Actions.tsx
+++ IMS-FRONTEND/src/pages/Actions.tsx
@@ -16,23 +16,28 @@ export const Actions = () => {
 
 
   return (
-    <div className='main-actions'>
-        <div className='input-actions'>
-            <Dropdown>
-              <Dropdown.Toggle id="dropdown-autoclose-true" className="dropdown-button-custom">Choose an Action</Dropdown.Toggle>
-              <Dropdown.Menu className="dropdown-menu-custom">
-                <Dropdown.Item as="button" onClick={() => setGetInventory(true)} className="dropdown-item-custom">Retrieve Inventory</Dropdown.Item>
-                <Dropdown.Item as="button" onClick={() => setDeleteInventory(true)} className="dropdown-item-custom">Delete Inventory</Dropdown.Item>
-                <Dropdown.Item as="button" onClick={() => setAddInventory(true)} className="dropdown-item-custom">Add Iventory</Dropdown.Item>
-                <DropdownDivider/>
-                <Dropdown.Item as="button" onClick={() => setUpdateItem(true)} className="dropdown-item-custom">Update Item</Dropdown.Item>
-                <Dropdown.Item as="button" onClick={() => setDeleteItem(true)} className="dropdown-item-custom">Delete Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-        </div>
-        <div>
-          Check action state variables here, render based on value.
-        </div>
+  <div className='body'>
+    <div className='actions'>
+      <div className='main-actions'>
+          <div className='input-actions'>
+              <Dropdown>
+                <Dropdown.Toggle id="dropdown-autoclose-true" className="dropdown-button-custom">Choose an Action</Dropdown.Toggle>
+                <Dropdown.Menu className="dropdown-menu-custom">
+                  <Dropdown.Item as="button" onClick={() => setGetInventory(true)} className="dropdown-item-custom">Retrieve Inventory</Dropdown.Item>
+                  <Dropdown.Item as="button" onClick={() => setDeleteInventory(true)} className="dropdown-item-custom">Delete Inventory</Dropdown.Item>
+                  <Dropdown.Item as="button" onClick={() => setAddInventory(true)} className="dropdown-item-custom">Add Iventory</Dropdown.Item>
+                  <DropdownDivider/>
+                  <Dropdown.Item as="button" onClick={() => setUpdateItem(true)} className="dropdown-item-custom">Update Item</Dropdown.Item>
+                  <Dropdown.Item as="button" onClick={() => setDeleteItem(true)} className="dropdown-item-custom">Delete Item</Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+          </div>
+          <div>
+            Check action state variables here, render based on value.
+          </div>
+      </div>
+      <div className='arrow-actions'>Arrow Icon</div>
     </div>
+  </div>
   )
 }
